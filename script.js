c9844e4ec36d1231ab892f9fcd7f07c7ac43fb46
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-06-20 13:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault();
+	const radius = document.getElementById('radius');
+	const volume = document.getElementById('volume');
+
+	if(Number(radius.value) > 0 && Number(radius.value) !== typeof ''){
+		let vol = (4 / 3) * Math.PI * Math.pow((radius.value), 3);
+
+		volume.value = vol.toFixed(4);
+	}else{
+		return NaN; 
+	}
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
